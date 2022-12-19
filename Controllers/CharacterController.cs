390b238bf50290d44ad929c16bd74ec6e9824021
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2022-12-19 20:44:17 +0800

    adding proper service response with generics.

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -18,19 +18,19 @@ namespace RPGApi.Controllers
         }
 
         [HttpGet("GetAll")]
-        public async Task<ActionResult<List<Character>>> Get()
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
         {
             return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<List<Character>>> GetSingle(int id)
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> GetSingle(int id)
         {
             return Ok(await _characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
         {
             return Ok(await _characterService.AddCharacter(newCharacter));
         }
