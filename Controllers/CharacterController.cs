8ca825896912bcb5f67ffb2a1073f17ff93bfa1b
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2022-12-19 20:16:56 +0800

    make asynchronous

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -20,19 +20,19 @@ namespace RPGApi.Controllers
         [HttpGet("GetAll")]
         public async Task<ActionResult<List<Character>>> Get()
         {
-            return Ok(_characterService.GetAllCharacters());
+            return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<List<Character>>> GetSingle(int id)
         {
-            return Ok(_characterService.GetCharacterById(id));
+            return Ok(await _characterService.GetCharacterById(id));
         }
 
         [HttpPost]
         public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter)
         {
-            return Ok(_characterService.AddCharacter(newCharacter));
+            return Ok(await _characterService.AddCharacter(newCharacter));
         }
     }
 }
